911c2d00e548f3ac64c536ae9da6869122170659
Minko <mf0981115811@gmail.com>
2024-04-05 09:32:10 +0300

    count sentences function

--- main.py
+++ main.py
@@ -4,6 +4,14 @@ def count_words(text):
     words = re.findall(r'\b\w+\b', text)
     return len(words)
 
+def count_sentences(text):
+    sentences = text.split('.')
+    sentences += text.split('!')
+    sentences += text.split('?')
+    # видалення пробілів та пустих рядків
+    sentences = [sentence.strip() for sentence in sentences if sentence.strip()]
+    return len(sentences)
+
 filename = 'text.txt'
 
 with open(filename, 'r', encoding='utf-8') as file:
