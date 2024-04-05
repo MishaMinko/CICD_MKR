9f7cbdbac429b679f7fc3566593e45a69cc60ef5
Minko <mf0981115811@gmail.com>
2024-04-05 09:27:32 +0300

    count words function

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
+import re
+
+def count_words(text):
+    words = re.findall(r'\b\w+\b', text)
+    return len(words)
+
 filename = 'text.txt'
 
 with open(filename, 'r', encoding='utf-8') as file:
-    content = file.read()
+    text = file.read()
